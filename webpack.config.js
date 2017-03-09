e46a396dc6af02c4732d01eb803a84b7a80b31d2
GerritSe <gerrit.seger@gmail.com>
2017-03-09 23:53:02 +0100

    Use babel for transpiling ES6 to ES5

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: 'assets'
+  },
+  module: {
+    rules:[{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }]
+    }]
   }
 }
 
